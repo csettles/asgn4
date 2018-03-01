27eb7906f4778f3368ee1ce5c3553f305b9efd83
Donald Loveland <dlovelan@calpoly.edu>
2018-02-28 20:35:48 -0800

    Fixed printing

--- asgn4/dir_tree.c
+++ asgn4/dir_tree.c
@@ -342,7 +342,8 @@ void print_name(tar_header *th) {
 	if (strlen((const char *)th->prefix) > 0) {
 		printf("%s/%s", th->prefix, th->name);
 	} else {
-		printf("%s", th->name);
+		/* For some reason his tests only print the first 100.. idk? */ 
+		printf("%.*s", 100, th->name);
 	}
 	printf("\n");
 }
